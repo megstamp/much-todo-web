4f43249f58fb13644fe966e7ad40529cd15144e8
Meghann Stamper <meg.stamp@yahoo.com>
2023-04-21 13:03:44 -0400

    don't understand

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react"
+import { List }  from "antd"
+
+export default function TodoList({ loading, itemList, setItemList, setLoading }) {
+
+    useEffect(() => {
+        fetch("https://much-todo-app-ms.web.app/items")
+        .then(resp => resp.json())
+        .then(setItemList)
+        .catch(alert)
+        .finally(() => setLoading(false))
+    }, [])
+
+    return (
+        <section>
+            <List 
+                bordered
+                dataSource={itemList}
+                loading={loading}
+                size="large"
+                renderItem={(task) => (
+                <List.Item className={(task.done) && "done"}>
+                    {task.item}
+                </List.Item>
+             ) }
+             />
+        </section>
+    )
+
+}
